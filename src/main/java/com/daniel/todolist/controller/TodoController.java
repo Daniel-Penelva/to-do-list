c8e904884d662645af6f33e75dc3ed8bbb8bf868
Daniel Penelva <d4n.andrade@gmail.com>
2023-09-01 23:27:38 -0300

    Criando o TodoController

---
+++ src/main/java/com/daniel/todolist/controller/TodoController.java
@@ -0,0 +1,52 @@
+package com.daniel.todolist.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.daniel.todolist.entity.Todo;
+import com.daniel.todolist.service.TodoService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/todos")
+@AllArgsConstructor
+public class TodoController {
+
+    private TodoService todoService;
+
+    // http://localhost:8080/todos
+    @GetMapping
+    List<Todo> list(){
+        return todoService.list();
+    }
+
+
+     // http://localhost:8080/todos
+    @PostMapping
+    List<Todo> create(@RequestBody Todo todo){
+        return todoService.create(todo);
+    }
+
+
+    // http://localhost:8080/todos
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo){
+        return todoService.update(todo);
+    }
+
+
+    // http://localhost:8080/todos/{id}
+    @DeleteMapping("/{id}")
+    List<Todo> delete(@PathVariable("id") Long id){
+        return todoService.delete(id);
+    }
+}
